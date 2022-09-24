80167dd4e409633661bfd4c79fb78f7a2a343d03
Tewzzz <imannuel2215@gmail.com>
2022-09-24 11:34:12 +0700

    first commit

---
+++ PBO-TI-21-PA/src/Latihan7.java
@@ -0,0 +1,10 @@
+public class Latihan7 {
+	
+	public static void main(String[] args) {
+		
+		String data = "Matthew";
+		data = data.replaceAll("[A,a,i,u,e,o]", "X");
+		System.out.println("Matthew ==>" + data);
+	}	    
+
+}
